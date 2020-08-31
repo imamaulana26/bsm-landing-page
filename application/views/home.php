e89445e6181adb2dd59807151a9671b0e07e3dfc
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-08-31 11:26:04 +0700

    change background jumbotron

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,8 @@
 <!-- Jumbotron -->
 <div class="jumbotron jumbotron-fluid">
 	<div class="container">
-		<h1 class="display-4">Get work done <span>faster</span><br>and <span>better</span> with us</h1>
-		<a href="" class="btn btn-primary tombol">OUR WORK</a>
+		<!-- <h1 class="display-4">Bank Syariah <br><span>Terdepan</span> dan <span>Modern</span></h1>
+		<a href="" class="btn btn-primary tombol">OUR WORK</a> -->
 	</div>
 </div>
 <!-- End of Jumbotron -->
